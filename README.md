82514cebdf6ef0a5334872439eb68d79d61d5c8e
jisung <jisung0920@gmail.com>
2019-10-01 12:55:17 +0900

    cds code

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Cambridge-Dictionary-Scraper
-English to English scraper
+
+
+# Cambridge Dictionary E to E Scraper
+
+> 2019.10.1.
+>
+> Jisung Jeong (Hanyang Univ.)
+
+
+
+## What is CDS
+
+**CDS** is a file generator that find definitions of input word. (file form)
+
+You can easily get the definitions you want to find. (English to English)
+
+
+
+## How to use
+
+1. make a file and write words per line.
+
+   > input file
+   > <img src="assets/image-20191001124125480.png" alt="image-20191001124125480" style="zoom:50%;" />
+
+2. CLI
+
+   ```
+   python cds.py 'input file name' 'output file name' 'the number of definitions'
+   ```
+
+   > output file: 
+   > <img src="assets/image-20191001124722564.png" alt="image-20191001124722564" style="zoom:50%;" />
+
---
+++ cds.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+CAMBRIDGE_URL = "http://dictionary.cambridge.org/dictionary/english"
+
+
+def usage():
+    print("Usage: {name} 1.inputFileName 2.outputFileName 3.NumberOfDefines".format(name=sys.argv[0]))
+    sys.exit(1)
+
+
+def main(vocaFileName,outputFileName,defSize):
+    try :
+        vocaFile = open(vocaFileName,'r')
+        outFile = open(outputFileName,'w')
+
+        vocas =vocaFile.readlines()
+    except :
+        print("check your file name")
+        exit()
+
+    for voca in vocas :
+        try :
+            camUrl = urlopen("/".join([CAMBRIDGE_URL, voca]))
+            soup = BeautifulSoup(camUrl, 'html.parser')
+            defines = soup.findAll('div',class_='ddef_h')
+        
+
+            outFile.write(voca+"=")
+
+            i = 0
+            while  i<int(defSize) :
+                try : 
+                    outFile.write(defines[i].text)
+                    i += 1
+                except :
+                    break
+            outFile.write("\n\n")
+        except :
+            break
+    vocaFile.close()
+    outFile.close()
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        usage()
+    main(sys.argv[1], sys.argv[2],sys.argv[3])
